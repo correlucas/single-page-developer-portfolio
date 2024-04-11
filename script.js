d6fd1267da81dc9469642f6455bae42104e12698
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-04-11 12:35:49 -0300

    js fixes

--- script.js
+++ script.js
@@ -1,50 +1,20 @@
 const form = document.querySelector('form');
-const inputs = document.querySelectorAll('input');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
 const button = document.getElementById('button');
+const formFields = document.querySelectorAll(".required") // Captura todos os campos (2 inputs e 1 textarea)
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const email = emailInput.value.trim();
-  const name = nameInput.value.trim();
-  const message = messageInput.value.trim();
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  formFields.forEach((field) => {
+    const errorIcon = field.closest(".form-test").querySelector(".icon-error"); // Captura o icone de erro daquele campo
+    const errorMessage = field.closest(".form-test").querySelector(".error-message"); // Captura a mensagem de erro daquele campo
 
-  const nameError = document.getElementById('error-name');
-  const nameIconError = nameInput.closest(".form-test").querySelector("img");
-  const emailError = document.getElementById('error-email');
-  const emailIconError = emailInput.closest(".form-test").querySelector("img");
-  const messageError = document.getElementById('error-message');
-  const messageIconError = messageInput.closest(".form-test").querySelector("img");
-
-  if (name === "") {
-    nameError.style.opacity = '1';
-    nameIconError.style.display = "block"
-  } else {
-    nameError.style.opacity = '0';
-    nameIconError.style.display = "none"
-  }
-  
-  if (email === "") {
-    emailError.style.opacity = '1';
-    emailIconError.style.display = "block"
-  } else {
-    emailError.style.opacity = '0';
-    emailIconError.style.display = "none"
-  }
-  
-  if (message === "") {
-    messageError.style.opacity = '1';
-    messageIconError.style.display = "block"
-  } else {
-    messageError.style.opacity = '0';
-    messageIconError.style.display = "none"    
-  }
-})
-
-name.addEventListener('input', function () {
-  nameError.style.opacity = '0';
+    if (field.value === '') {
+      errorIcon.style.display = 'block';
+      errorMessage.style.opacity = '1';
+    } else {
+      errorIcon.style.display = 'none';
+      errorMessage.style.opacity = '0';
+    }
+  })
 })
