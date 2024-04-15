db87f4bfff8d9c786cf154b8a28ae972f94701fb
Lucas <71549267+correlucas@users.noreply.github.com>
2024-04-15 22:21:20 +0200

    email input message alert

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const button = document.getElementById('button');
 
+
+const nameError = document.getElementById('error-name');
+const emailError = document.getElementById('error-email');
+const messageError = document.getElementById('error-message');
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -12,10 +17,6 @@ form.addEventListener("submit", function (event) {
   const name = nameInput.value.trim();
   const message = messageInput.value.trim();
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  const nameError = document.getElementById('error-name');
-  const emailError = document.getElementById('error-email');
-  const messageError = document.getElementById('error-message');
 
   if (nameInput.value.trim() === "") {
     nameError.style.opacity = '1';
@@ -25,11 +26,21 @@ form.addEventListener("submit", function (event) {
     emailError.style.opacity = '1';
   }
 
-  if (emailInput.value.trim() === "") {
+  if (messageInput.value.trim() === "") {
     messageError.style.opacity = '1';
   }
 })
 
-name.addEventListener('input', function () {
+// Disable alert when something is typed in the invalid field 
+emailInput.addEventListener('input', function () {
+  emailError.style.opacity = '0';
+});
+
+messageInput.addEventListener('input', function () {
+  messageError.style.opacity = '0';
+});
+
+nameInput.addEventListener('input', function () {
   nameError.style.opacity = '0';
-})
+});
+
