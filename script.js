d82989c18c438b2a1085860cf4fe33203e6570b5
Lucas <71549267+correlucas@users.noreply.github.com>
2024-04-23 23:35:01 +0200

    working

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const messageInput = document.getElementById('message');
 const button = document.getElementById('button');
 
 
+// BLOCO DE ICONES SELECIONADOS POR ID CLASSE
+const nameIcon = document.querySelector('.icon-name');
+const emailIcon = document.querySelector('.icon-email');
+const messageIcon = document.querySelector('.icon-message');
+
+// BLOCO DE MENSAGEM DE ERROR SELECIONADAS POR ID INDIVIDUALMENTE 
 const nameError = document.getElementById('error-name');
 const emailError = document.getElementById('error-email');
 const messageError = document.getElementById('error-message');
@@ -21,14 +27,17 @@ form.addEventListener("submit", function (event) {
 
   if (nameInput.value.trim() === "") {
     nameError.style.opacity = '1';
+    nameIcon.style.display = 'block';
   }
 
   if (emailInput.value.trim() === "") {
     emailError.style.opacity = '1';
+    emailIcon.style.display = 'block';
   }
 
   if (messageInput.value.trim() === "") {
     messageError.style.opacity = '1';
+    messageIcon.style.display = 'block';
   }
 
   if (!emailRegex.test(email)) {
@@ -41,15 +50,20 @@ form.addEventListener("submit", function (event) {
 })
 
 // Disable alert when something is typed in the invalid field 
+nameInput.addEventListener('input', function () {
+  nameError.style.opacity = '0';
+  nameIcon.style.display = 'none';
+});
+
 emailInput.addEventListener('input', function () {
   emailError.style.opacity = '0';
+  emailIcon.style.display = 'none';
 });
 
 messageInput.addEventListener('input', function () {
   messageError.style.opacity = '0';
+  messageIcon.style.display = 'none';
 });
 
-nameInput.addEventListener('input', function () {
-  nameError.style.opacity = '0';
-});
+
 
